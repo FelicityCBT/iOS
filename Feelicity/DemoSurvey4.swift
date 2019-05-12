0295fe3f1ea5f59c13640c90b7d460ad28cdaba0
George <georgewpu@gmail.com>
2019-05-12 00:02:50 -0700

    Uploading Demo Survey

---
+++ Feelicity/DemoSurvey4.swift
@@ -0,0 +1,28 @@
+//
+//  DemoSurvey.swift
+//  Feelicity
+//
+//  Created by George Pu on 2/28/19.
+//  Copyright © 2019 Feelicity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class DemoSurvey4: UIViewController {
+    override func viewDidLoad() {
+        
+        // 3
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.contentMode = .scaleAspectFit
+        
+        // 4
+        let image = UIImage(named: "SunIcon")
+        imageView.image = image
+        
+        // 5
+        navigationItem.titleView = imageView
+    }
+}
