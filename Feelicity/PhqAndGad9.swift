bc8e2e6a18d770cc3e6d6642a3bd1f31c5a2332d
George <georgewpu@gmail.com>
2019-05-14 22:13:27 -0700

    Working on the phq survey checkpoint

---
+++ Feelicity/PhqAndGad9.swift
@@ -0,0 +1,20 @@
+//
+//  PhqAndGad9.swift
+//  Feelicity
+//
+//  Created by George Pu on 2/27/19.
+//  Copyright © 2019 Feelicity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class PhqAndGad9: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
